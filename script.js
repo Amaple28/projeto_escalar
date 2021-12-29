fbc65885bdbb2f8107aec98dffead0f4d799f366
amaple28 <maisagabirodrigues@gmail.com>
2021-12-29 20:45:19 -0300

    alterações js

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// var produtos = [
-//     { nome: "tv1", preco: "", quantidade: "" },
-//     { nome: "tv2", preco: "", quantidade: "" },
-//     { nome: "tv3", preco: "", quantidade: "" },
-//     { nome: "tv4", preco: "", quantidade: "" },
-//     { nome: "tv5", preco: "", quantidade: "" },
-//     { nome: "tv6", preco: "", quantidade: "" },
-//     { nome: "tv7", preco: "", quantidade: "" },
-//     { nome: "tv8", preco: "", quantidade: "" },
-//     { nome: "tv9", preco: "", quantidade: "" },
-//     { nome: "tv10", preco: "", quantidade: "" },
-//     { nome: "tv11", preco: "", quantidade: "" },
-//     { nome: "player1", preco: "", quantidade: "" },
-//     { nome: "player2", preco: "", quantidade: "" },
-//     { nome: "player3", preco: "", quantidade: "" },
-// ];
-
 //Regras de negócio
+
+//JSON com todos os produtos
 var produtos = [
     { nome: "TV 32 Polegadas", preco: 0, quantidade: 0 },
     { nome: "TV 40 Polegadas", preco: 0, quantidade: 0 },
@@ -35,40 +20,101 @@ var produtos = [
         quantidade: 0,
     },
     { nome: "TV Box", preco: 0, quantidade: 0 },
+    { nome: "Planos", preco: 0, quantidade: 0 },
+    { nome: "Mão de obra", preco: 0, quantidade: 1 },
 ];
 
-function finalizarCompra() {
+//Função para finalizar orçamento
+function finalizarOrcamento() {
     let quantidades = document.querySelectorAll(".input-quantidade");
     let precos = document.querySelectorAll(".input-preco");
     let inputTotalInvestimento = document.querySelector(
         ".input-total-investimento"
     );
-    console.log(quantidades);
     let totalInvestimento = 0;
+    let inputTotalMensalidade = document.querySelector(
+        ".input-total-mensalidade"
+    );
+    let planos = document.querySelectorAll(".form-check-input");
+    let quantidadePlanos = document.querySelector(".input-quantidade-planos");
+    produtos[15].preco = parseFloat(
+        document.querySelector(".input-preco-obra").value
+    );
+
+    planos.forEach((plano) => {
+        if (plano.checked) {
+            produtos[14].preco = parseFloat(plano.value);
+            console.log(quantidadePlanos);
+            produtos[14].quantidade = parseFloat(quantidadePlanos.value);
+        }
+    });
 
     for (let i = 0; i < quantidades.length; i++) {
-        console.log(produtos[i].nome);
-
         produtos[i].quantidade =
             quantidades[i].value != "" ? parseFloat(quantidades[i].value) : 0;
         produtos[i].preco = precos[i].value != "" ? parseFloat(precos[i].value) : 0;
     }
 
-    produtos.forEach((produto) => {
-        totalInvestimento += produto.quantidade * produto.preco;
-    });
+    console.log(produtos);
 
+    produtos.forEach((produto) => {
+        if (produto.nome != "Planos") {
+            totalInvestimento += produto.quantidade * produto.preco;
+        } else {
+            totalInvestimento += produto.preco;
+        }
+    });
     inputTotalInvestimento.value = totalInvestimento;
+    inputTotalMensalidade.value = produtos[14].preco * produtos[14].quantidade;
 }
 
-function finalizarPlanos(value) {
-    let inputTotalMensalidade = document.querySelector(
-        ".input-total-mensalidade"
-    );
-    let inputQuantidadePlanos = document.querySelector(
-        ".input-quantidade-planos"
-    );
+//Funções para alterar valores em tempo real
+function liveChange(name) {
+    document.getElementById(name + "-total").value =
+        parseFloat(document.getElementById(name + "-preco").value) *
+        parseFloat(document.getElementById(name + "-quantidade").value);
+}
 
-    inputTotalMensalidade.value =
-        parseFloat(value) * parseFloat(inputTotalMensalidade.value);
+function setPlanoSelecionado(value) {
+    document.getElementById("input-quantidade-planos").name = value;
+}
+
+function liveChangePlanos(value) {
+    document.getElementById("input-total-planos").value =
+        parseFloat(document.getElementById("gridRadios" + value).value) *
+        parseFloat(document.getElementById("input-quantidade-planos").value);
+}
+
+function formaDePagamento(value) {
+    let bronze = document.getElementById("gridRadios39");
+    let prata = document.getElementById("gridRadios49");
+    let ouro = document.getElementById("gridRadios59");
+    let diamante = document.getElementById("gridRadios69");
+
+    let bronzeLabel = document.getElementById("gridRadios39-label");
+    let prataLabel = document.getElementById("gridRadios49-label");
+    let ouroLabel = document.getElementById("gridRadios59-label");
+    let diamanteLabel = document.getElementById("gridRadios69-label");
+
+    if ((value = "Anual")) {
+        bronze.value = 15.9;
+        prata.value = 19.6;
+        ouro.value = 23.01;
+        diamante.value = 26.67;
+
+        bronzeLabel.innerHTML = "Plano Bronze | R$ 15,9";
+        prataLabel.innerHTML = "Plano Prata | R$ 19,6";
+        ouroLabel.innerHTML = "Plano Ouro | R$ 23,01";
+        diamanteLabel.innerHTML = "Plano Diamante | R$ 26,67";
+    } else {
+        bronze.value = 39;
+        prata.value = 49;
+        ouro.value = 59;
+        diamante.value = 69;
+
+        bronzeLabel.innerHTML = "Plano Bronze | R$ 39,00";
+        prataLabel.innerHTML = "Plano Prata | R$ 49,00";
+        ouroLabel.innerHTML = "Plano Ouro | R$ 59,00";
+        diamanteLabel.innerHTML = "Plano Diamante | R$ 69,00";
+    }
 }
