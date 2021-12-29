0a41876b665e63064a0fb11707c854ec845bb6a4
amaple28 <maisagabirodrigues@gmail.com>
2021-12-29 18:07:29 -0300

    alterações js

--- script.js
+++ script.js
@@ -1,43 +1,74 @@
-//carrega pacote dos gráficos basicos
-google.charts.load('current', { 'packages': ['corechart'] });
+// var produtos = [
+//     { nome: "tv1", preco: "", quantidade: "" },
+//     { nome: "tv2", preco: "", quantidade: "" },
+//     { nome: "tv3", preco: "", quantidade: "" },
+//     { nome: "tv4", preco: "", quantidade: "" },
+//     { nome: "tv5", preco: "", quantidade: "" },
+//     { nome: "tv6", preco: "", quantidade: "" },
+//     { nome: "tv7", preco: "", quantidade: "" },
+//     { nome: "tv8", preco: "", quantidade: "" },
+//     { nome: "tv9", preco: "", quantidade: "" },
+//     { nome: "tv10", preco: "", quantidade: "" },
+//     { nome: "tv11", preco: "", quantidade: "" },
+//     { nome: "player1", preco: "", quantidade: "" },
+//     { nome: "player2", preco: "", quantidade: "" },
+//     { nome: "player3", preco: "", quantidade: "" },
+// ];
 
-//definimos a funcao de desenhar como callback do carregamento do pacote
-google.charts.setOnLoadCallback(desenha_grafico);
+//Regras de negócio
+var produtos = [
+    { nome: "TV 32 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "TV 40 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "TV 43 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "TV 49 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "TV 50 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "TV 55 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "TV 58 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "TV 65 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "TV 75 Polegadas", preco: 0, quantidade: 0 },
+    { nome: "Monitor LED", preco: 0, quantidade: 0 },
+    { nome: "Monitor video Wall", preco: 0, quantidade: 0 },
+    { nome: "Player Alphasignage", preco: 0, quantidade: 0 },
+    {
+        nome: "Player Alphasignage Secundário",
+        preco: 0,
+        quantidade: 0,
+    },
+    { nome: "TV Box", preco: 0, quantidade: 0 },
+];
 
-//funcao que configura e desenha grafico
-function desenha_grafico() {
+function finalizarCompra() {
+    let quantidades = document.querySelectorAll(".input-quantidade");
+    let precos = document.querySelectorAll(".input-preco");
+    let inputTotalInvestimento = document.querySelector(
+        ".input-total-investimento"
+    );
+    console.log(quantidades);
+    let totalInvestimento = 0;
 
-    //cria dataTable com os dados
-    var dados = new google.visualization.DataTable();
-    dados.addColumn('string', 'Escolaridade');
-    dados.addColumn('number', 'Anos');
-    dados.addRows([
-        ['Primário', 9],
-        ['Ensino Médio', 3],
-        ['Faculdade', 4],
-        ['Mestrado', 2],
-        ['Doutorado', 4]
-    ]);
+    for (let i = 0; i < quantidades.length; i++) {
+        console.log(produtos[i].nome);
 
-    var altura = 250;
-
-    var largura = 600;
-    if (document.getElementById('grafico_div').offsetWidth < 600) {
-        largura = document.getElementById('grafico_div').offsetWidth;
+        produtos[i].quantidade =
+            quantidades[i].value != "" ? parseFloat(quantidades[i].value) : 0;
+        produtos[i].preco = precos[i].value != "" ? parseFloat(precos[i].value) : 0;
     }
 
-    //configurações do grafico
-    var opcoes = {
-        'title': 'Quantidade de anos em cada período escolar',
-        'width': largura,
-        'height': altura
-    };
+    produtos.forEach((produto) => {
+        totalInvestimento += produto.quantidade * produto.preco;
+    });
 
-    //define grafico como gráfico de pizza a ser incluido no elemento de id grafico_div
-    var grafico = new google.visualization.ColumnChart(document.getElementById('grafico_div'));
-
-    //desenha gráfico com dados e configuraçoes definidas
-    grafico.draw(dados, opcoes);
+    inputTotalInvestimento.value = totalInvestimento;
 }
 
-window.addEventListener('resize', desenha_grafico);
+function finalizarPlanos(value) {
+    let inputTotalMensalidade = document.querySelector(
+        ".input-total-mensalidade"
+    );
+    let inputQuantidadePlanos = document.querySelector(
+        ".input-quantidade-planos"
+    );
+
+    inputTotalMensalidade.value =
+        parseFloat(value) * parseFloat(inputTotalMensalidade.value);
+}
